ee539da56b855a4809ea2b4784819a827c2508ef
Anjali Deshmukh <anjalideshmukh2275@gmail.com>
2026-09-20 22:34:48 +0530

    Friend Requests II: Who Has the Most Friends

---
+++ Medium/leetcode_602.sql
@@ -0,0 +1,13 @@
+select id, count(*) as num
+from (
+    select requester_id as id
+    from requestaccepted
+
+    union all
+
+    select accepter_id as id
+    from requestaccepted
+) as friends
+group by id
+order by num desc
+limit 1;
